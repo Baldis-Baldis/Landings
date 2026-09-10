359ed07c82ed7ddad23ff44a3f04a4483f519753
Baldis-Baldis <ynwkvwnz5w@privaterelay.appleid.com>
2026-09-10 21:08:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
-const C='landings-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
+const C='landings-v3';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])));});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('tile.openstreetmap.org')) return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'&&res.ok){const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));}return res;})));
+  const u=e.request.url;
+  if(e.request.method!=='GET'||u.includes('tile.openstreetmap.org')) return;
+  // network first: always try fresh copy, fall back to cache when offline
+  e.respondWith(fetch(e.request).then(res=>{
+    if(res.ok){const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));}
+    return res;
+  }).catch(()=>caches.match(e.request)));
 });
